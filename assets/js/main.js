8f02f45a85b69cc0d0f8f2af870246602913dc67
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-06-21 18:36:41 -0400

    Feature: Componente Header and Nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
 /* ===================== HEADER ===================== */
 
-// REMOVE MENU MOBILE
+// SHOW MENU
+const navMenu = document.getElementById('nav_menu'),
+    navToggle = document.getElementById('nav_toggle'),
+    navClose = document.getElementById('nav_close');
 
+    if(navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show_menu');
+        })
+    }
+
+    if(navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show_menu');
+        })
+    }
+
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav_link');
+const linkAction = () => {
+    const navMenu = document.getElementById('nav_menu');
+    navMenu.classList.remove('show_menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // ADD BLUR HEADER
 
