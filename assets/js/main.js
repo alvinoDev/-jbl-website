e50a38399142a358e0a84174cac7d173bbcdf404
alvinoDev <48383924+alvinoDev@users.noreply.github.com>
2024-06-26 10:22:28 -0400

    Feature: Add blur header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,36 +2,41 @@
 
 // SHOW MENU
 const navMenu = document.getElementById('nav_menu'),
-    navToggle = document.getElementById('nav_toggle'),
-    navClose = document.getElementById('nav_close');
+	navToggle = document.getElementById('nav_toggle'),
+	navClose = document.getElementById('nav_close');
 
-    if(navToggle) {
-        navToggle.addEventListener('click', () => {
-            navMenu.classList.add('show_menu');
-        })
-    }
+if (navToggle) {
+	navToggle.addEventListener('click', () => {
+		navMenu.classList.add('show_menu');
+	});
+}
 
-    if(navClose) {
-        navClose.addEventListener('click', () => {
-            navMenu.classList.remove('show_menu');
-        })
-    }
+if (navClose) {
+	navClose.addEventListener('click', () => {
+		navMenu.classList.remove('show_menu');
+	});
+}
 
 // REMOVE MENU MOBILE
 const navLink = document.querySelectorAll('.nav_link');
 const linkAction = () => {
-    const navMenu = document.getElementById('nav_menu');
-    navMenu.classList.remove('show_menu');
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+	const navMenu = document.getElementById('nav_menu');
+	navMenu.classList.remove('show_menu');
+};
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // ADD BLUR HEADER
-
+const blurHeader = () => {
+	const header = document.getElementById('header');
+	this.scrollY >= 50
+		? header.classList.add('blur_header')
+		: header.classList.remove('blur_header');
+};
+window.addEventListener('scroll', blurHeader);
 
 // SWIPER FAVORITES
 
 /* ====================== MAIN ====================== */
-
 
 /* ================= SCROLL ACTIONS ================= */
 
@@ -40,6 +45,5 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 // SCROLL SECTIONS ACTIVE LINK
 
 // SCROLL REVEAL ANIMATION
-
 
 /* ===================== FOOTER ===================== */
